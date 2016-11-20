dd9b04c8f8312d9094cfd42313ab1ffd2993d6c9
Mrucznik <mrucznix@gmail.com>
2016-11-20 23:14:08 +0100

    Full Complex class

--- complex/complex.cpp
+++ complex/complex.cpp
@@ -1,3 +1,116 @@
-#include "complex.hpp"
+#include <cmath>
+#include "Complex.h"
 
+Complex::Complex()
+{
+}
+
+Complex::Complex(double real) :
+        real(real)
+{
+}
+
+Complex::Complex(double real, double imaginary) :
+        real(real), imaginary(imaginary)
+{
+}
+
+Complex &Complex::operator=(const Complex& c)
+{
+    real = c.real;
+    imaginary = c.imaginary;
+    return *this;
+}
+
+Complex &Complex::operator=(const double d)
+{
+    real = d;
+    imaginary = 0;
+    return *this;
+}
+
+Complex &Complex::operator+=(Complex c)
+{
+    real += c.real;
+    imaginary += c.imaginary;
+    return *this;
+}
+
+Complex &Complex::operator-=(Complex c)
+{
+    real -= c.real;
+    imaginary -= c.imaginary;
+    return *this;
+}
+
+Complex &Complex::operator*=(Complex c)
+{
+    real = real*c.real - imaginary*c.imaginary;
+    imaginary = imaginary*c.real - real*c.imaginary;
+    return *this;
+}
+
+Complex &Complex::operator/=(Complex c)
+{
+    double divisor = imaginary*imaginary + c.imaginary*c.imaginary;
+    real = (real*c.real + imaginary*c.imaginary)/divisor;
+    imaginary = (imaginary*c.real + real*c.imaginary)/divisor;
+    return *this;
+}
+
+Complex Complex::operator+(const Complex& c) const
+{
+    return Complex(real+c.real, imaginary+c.imaginary);
+}
+
+Complex Complex::operator-(const Complex& c) const
+{
+    return Complex(real-c.real, imaginary-c.imaginary);
+}
+
+Complex Complex::operator*(const Complex& c) const
+{
+    return Complex(real*c.real - imaginary*c.imaginary, imaginary*c.real - real*c.imaginary);
+}
+
+Complex Complex::operator/(const Complex& c) const
+{
+    double divisor = imaginary*imaginary + c.imaginary*c.imaginary;
+    return Complex((real*c.real + imaginary*c.imaginary)/divisor, (imaginary*c.real + real*c.imaginary)/divisor);
+}
+
+Complex Complex::operator-() const
+{
+    return Complex(-real, -imaginary);
+}
+
+bool Complex::operator==(const Complex& c) const
+{
+    return real == c.real && imaginary == c.imaginary;
+}
+
+bool Complex::operator!=(const Complex& c) const
+{
+    return !(c == *this);
+}
+
+double Complex::getReal() const
+{
+    return real;
+}
+
+double Complex::getImaginary() const
+{
+    return imaginary;
+}
+
+double Complex::getAmplitude() const
+{
+    return sqrt(real*real + imaginary*imaginary);
+}
+
+double Complex::getPhase() const
+{
+    return atan2(imaginary, real);
+}
 
---
+++ complex/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Complex.h"
+
+using namespace std;
+
+int main()
+{
+    Complex a, b, c, d;
+    cout << "Hello, World!" << endl;
+    return 0;
+}
